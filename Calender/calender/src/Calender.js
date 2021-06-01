610e2d02a26e241819ad757753065c397781118c
basilbabu3196 <basilbabu3196@gmail.com>
2021-06-01 07:13:26 +0530

    1-6 12:50

--- Calender/calender/src/Calender.js
+++ Calender/calender/src/Calender.js
@@ -2,8 +2,43 @@ import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useHistory} from  'react-router-dom'
 import './calender.css';
+
 function Calender() {
-    
+    var dt = new Date();
+    dt.setDate(1);
+    var day = dt.getDay();
+    var today = new Date();
+    var endDate = new Date(
+        dt.getFullYear(),
+        dt.getMonth() + 1,
+        0
+    ).getDate();
+
+    var prevDate = new Date(
+        dt.getFullYear(),
+        dt.getMonth(),
+        0
+    ).getDate();
+    var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+   var monthdata = months[dt.getMonth()];
+   var days = dt.toDateString();
+    const myFunction = (event) => {
+        event.preventDefault();
+      
+    }
    
     return<center><div> <div className="card-header">
     <h1>CALENDER</h1>
@@ -14,8 +49,8 @@ function Calender() {
                         <span>&#10094;</span>
                     </div>
                         <div>
-                            <h2 id="month"></h2>
-                            <p id="date_str"></p>
+                            <h2 id="month">{monthdata}</h2>
+                            <p id="date_str">{days}</p>
                         </div>
                     <div class="next" >
                         <span>&#10095;</span>
@@ -39,7 +74,7 @@ function Calender() {
     </div>
     <div id="id01" class="modal">
             
-            <form class="modal-content animate" action="/action_page.php" method="post">
+            <form class="modal-content animate" onSubmit={myFunction} method="post">
                 <div class="imgcontainer">EVENTS
                     <span class="close" title="Close Modal">&times;</span>
                 
